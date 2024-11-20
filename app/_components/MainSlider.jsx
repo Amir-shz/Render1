019ecba4ae5253042b616178fe254c106ff0ba8f
amir-shz <amirsharif2079@gmail.com>
2024-11-20 12:25:41 +0330

    add video to first slide

--- app/_components/MainSlider.jsx
+++ app/_components/MainSlider.jsx
@@ -8,6 +8,7 @@ import MainSlide from "./MainSlide";
 
 import slide1 from "@/public/slide1.jpg";
 import slide2 from "@/public/slide2.jpg";
+
 import NextBtn from "./NextBtn";
 import PrevBtn from "./PrevBtn";
 
@@ -26,16 +27,42 @@ function MainSlider() {
           parallax={true}
           centeredSlides={true}
           loop={true}
-          autoplay={{ delay: 4000, disableOnInteraction: false }}
+          autoplay={{ delay: 4500, disableOnInteraction: false }}
           modules={[Autoplay, EffectFade]}
           className="h-full"
         >
+          <SwiperSlide>
+            <video
+              src="slide3.mp4"
+              width="100px"
+              height="100px"
+              autoPlay
+              muted
+              loop
+              preload="none"
+              className="size-fit sepia-[0.05] brightness-[0.55] -z-10"
+            ></video>
+
+            <div className=" absolute top-0 left-0 size-full z-30 flex-col">
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-max max-[450px]:-translate-x-32">
+                <p className="text-primary font-semibold text-sm pb-4 max-md:text-sm max-[450px]:text-xs">
+                  سرورهای بهینه‌سازی شده برای سرعت و دقت
+                </p>
+                <p className="text-7xl font-semibold max-lg:text-5xl max-md:text-3xl max-[450px]:text-2xl">
+                  رندرهای بی‌نظیر با keyshot
+                </p>
+              </div>
+              <p className=" text-primary font-bold text-5xl absolute bottom-4 left-5">
+                01
+              </p>
+            </div>
+          </SwiperSlide>
           <SwiperSlide>
             <MainSlide
               src={slide1}
               title="سرورهای قوی برای رندر های واقع گرایانه"
               text="تجسم شگفت انگیز با lumion"
-              num="01"
+              num="02"
             />
           </SwiperSlide>
           <SwiperSlide>
@@ -43,12 +70,9 @@ function MainSlider() {
               src={slide2}
               title="سرورهای بهینه سازی شده برای رندرهای پیچیده"
               text="خلاقیت بی‌پایان با 3ds max"
-              num="02"
+              num="03"
             />
           </SwiperSlide>
-          {/* <SwiperSlide className=" sepia-[0.05] brightness-[0.55]">
-          3000
-          </SwiperSlide> */}
           <div className=" absolute bottom-0 right-16">
             <NextBtn />
             <PrevBtn />
