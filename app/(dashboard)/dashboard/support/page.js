cd748b71e2835237d09b97e74e5f30598e4394f5
amir-shz <amirsharif2079@gmail.com>
2024-12-06 23:40:03 +0330

    some ui change

--- app/(dashboard)/dashboard/support/page.js
+++ app/(dashboard)/dashboard/support/page.js
@@ -4,15 +4,18 @@ import { Button } from "@mui/material";
 function Page() {
   return (
     <div>
-      <h2 className=" border-b border-slate-400/50 pb-4 text-2xl text-slate-700 font-semibold m-4 max-sm:m-0 max-sm:mt-1  ">
+      <h2 className=" border-b border-slate-400/50 pb-4 text-2xl text-slate-700 font-semibold m-4 max-sm:m-0 max-sm:mt-1 max-sm:mb-4  ">
         پشتیبانی
       </h2>
       <div className="bg-slate-200 mx-4 rounded shadow-md shadow-slate-500/25 p-3  max-lg:p-2 max-sm:mx-0 ">
         <div className="bg-slate-100 rounded-sm p-4 shadow pb-2 max-sm:p-2">
           <p className=" text-slate-800 font-semibold">ایجاد تیکت پشتیبانی</p>
-          <form action={handleTicket} className="my-2 space-y-2 ">
-            <div className="grid grid-cols-2 items-center gap-2 max-lg:grid-cols-1">
-              <div className="grid grid-cols-[1fr_5fr] max-xl:grid-cols-[1fr_4fr] items-center gap-2">
+          <form
+            action={handleTicket}
+            className="my-2 space-y-2  max-[345px]:space-y-4 "
+          >
+            <div className="grid grid-cols-2 items-center gap-2 max-lg:grid-cols-1 max-[345px]:[&>div]:grid-cols-1  max-[345px]:gap-4  max-[345px]:[&>div]:gap-1">
+              <div className="grid grid-cols-[1fr_5fr] max-xl:grid-cols-[1fr_4fr] max-sm:grid-cols-[1fr_3fr] items-center gap-2">
                 <label htmlFor="ticketSubject" className=" max-sm:text-sm">
                   موضوع
                 </label>
@@ -25,7 +28,7 @@ function Page() {
                   className="py-2 px-4 focus:outline-none focus:border disabled:bg-slate-200 focus:border-slate-400 rounded-sm placeholder:text-slate-600 max-sm:placeholder:text-sm max-sm:px-2"
                 />
               </div>
-              <div className="grid grid-cols-[1fr_5fr] max-xl:grid-cols-[1fr_4fr] items-center gap-2">
+              <div className="grid grid-cols-[1fr_5fr] max-xl:grid-cols-[1fr_4fr] max-sm:grid-cols-[1fr_3fr] items-center gap-2">
                 <label htmlFor="ticketType" className=" max-sm:text-sm">
                   بخش
                 </label>
@@ -33,7 +36,7 @@ function Page() {
                   required
                   name="ticketType"
                   id="ticketType"
-                  className="py-2 px-4 focus:outline-none focus:border disabled:bg-slate-200 focus:border-slate-400 rounded-sm placeholder:text-slate-600 max-sm:placeholder:text-sm max-sm:px-2"
+                  className="py-2 px-4 focus:outline-none focus:border disabled:bg-slate-200 focus:border-slate-400 rounded-sm placeholder:text-slate-600 max-sm:placeholder:text-sm max-sm:px-2 max-sm:text-sm"
                 >
                   <option value="" hidden>
                     بخش مورد نظر را انتخاب کنید
@@ -43,7 +46,7 @@ function Page() {
                   <option value="Support">پشتیبانی</option>
                 </select>
               </div>
-              <div className="grid grid-cols-[1fr_5fr] max-xl:grid-cols-[1fr_4fr] items-center gap-2">
+              <div className="grid grid-cols-[1fr_5fr] max-xl:grid-cols-[1fr_4fr] max-sm:grid-cols-[1fr_3fr] items-center gap-2">
                 <label htmlFor="serviceCode" className=" max-sm:text-sm">
                   کد سرویس{" "}
                   <span className=" text-[10px] block">(در صورت وجود)</span>
@@ -57,7 +60,7 @@ function Page() {
                 />
               </div>
             </div>
-            <div className="grid grid-cols-[1fr_11fr] max-xl:grid-cols-[1fr_9fr] max-lg:grid-cols-[1fr_4fr] items-center gap-[7px]">
+            <div className="grid grid-cols-[1fr_11fr] max-xl:grid-cols-[1fr_9fr] max-lg:grid-cols-[1fr_4fr] items-center gap-[7px] max-sm:grid-cols-[1fr_3fr] max-sm:gap-2  max-[345px]:grid-cols-1">
               <label htmlFor="ticketText" className=" max-sm:text-sm">
                 درخواست
               </label>
