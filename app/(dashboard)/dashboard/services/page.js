6783520719977a59667ec6fc990bacc1ce5ea18e
amir-shz <amirsharif2079@gmail.com>
2024-12-07 14:58:09 +0330

    fix bug

--- app/(dashboard)/dashboard/services/page.js
+++ app/(dashboard)/dashboard/services/page.js
@@ -8,12 +8,13 @@ function Page() {
   const productNum = Array.from({ length: 22 });
 
   return (
-    <div>
-      <h2 className=" border-b border-slate-400/50 pb-4 text-2xl text-slate-700 font-semibold m-4 max-sm:m-0 max-sm:mt-1 ">
-        سرویس ها
-      </h2>
-      <div className=" grid grid-cols-4 gap-y-4 gap-x-2 max-xl:grid-cols-3 max-lg:grid-cols-2 max-lg:px-8 max-md:grid-cols-2 max-sm:grid-cols-1 max-md:px-0 max-sm:px-2 max-sm:mt-4">
-        {/* <ProductCard />
+    <Suspense>
+      <div>
+        <h2 className=" border-b border-slate-400/50 pb-4 text-2xl text-slate-700 font-semibold m-4 max-sm:m-0 max-sm:mt-1 ">
+          سرویس ها
+        </h2>
+        <div className=" grid grid-cols-4 gap-y-4 gap-x-2 max-xl:grid-cols-3 max-lg:grid-cols-2 max-lg:px-8 max-md:grid-cols-2 max-sm:grid-cols-1 max-md:px-0 max-sm:px-2 max-sm:mt-4">
+          {/* <ProductCard />
         <ProductCard isAvailable />
         <ProductCard />
         <ProductCard isAvailable />
@@ -21,14 +22,14 @@ function Page() {
         <ProductCard isAvailable />
         <ProductCard />
         <ProductCard isAvailable /> */}
-        <Suspense fallback="<p>loading</p>">
+
           <ProductList productNum={productNum} />
-        </Suspense>
+        </div>
+        <div className="flex justify-center my-4">
+          <Pagination count={productNum.length} />
+        </div>
       </div>
-      <div className="flex justify-center my-4">
-        <Pagination count={productNum.length} />
-      </div>
-    </div>
+    </Suspense>
   );
 }
 
